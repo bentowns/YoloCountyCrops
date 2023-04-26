f65010c615968fa3abd871af5f96993c90c28a5a
Ben Townsend <benctownsend98@gmail.com>
2023-04-26 11:15:44 -0400

    Updated csv file, adding python code

--- Code/3_ExploreViz.R
+++ Code/3_ExploreViz.R
@@ -8,45 +8,89 @@ source("Code/2_ExtractPixels.R")
 #-------------------------------------------------------------------------------
 #10m Band boxplots
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band2, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band2)) +
+  ggtitle("Band 2") +
+  xlab("Crop Type") +
+  ylab("Band 2 Surface Reflectance") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band3, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band3)) +
+  ggtitle("Band 3") +
+  xlab("Crop Type") +
+  ylab("Band 3 Surface Reflectance") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band4, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band4)) +
+  xlab("Crop Type") +
+  ylab("Band 4 Surface Reflectance") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band8, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band8)) +
+  xlab("Crop Type") +
+  ylab("Band 8 Surface Reflectance") +
+  theme_bw()
 
 #20m Band boxplots
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band6, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band6)) +
+  xlab("Crop Type") +
+  ylab("Band 6 Surface Reflectance") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band7, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band7)) +
+  xlab("Crop Type") +
+  ylab("Band 7 Surface Reflectance") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band11, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band11)) +
+  xlab("Crop Type") +
+  ylab("Band 11 Surface Reflectance") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band12, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band12)) +
+  xlab("Crop Type") +
+  ylab("Band 12 Surface Reflectance") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = Band8A, color = croptype))
+  geom_boxplot(aes(x = croptype, y = Band8A)) +
+  xlab("Crop Type") +
+  ylab("Band 8A Surface Reflectance") +
+  theme_bw()
 
 #Indices Boxplots
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = EVI, color = croptype))
+  geom_boxplot(aes(x = croptype, y = EVI)) +
+  xlab("Crop Type") +
+  ylab("Enhanced Vegetation Index") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = GCI, color = croptype))
+  geom_boxplot(aes(x = croptype, y = GCI)) +
+  xlab("Crop Type") +
+  ylab("Green Chlorophyll Index") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = NDTI, color = croptype))
+  geom_boxplot(aes(x = croptype, y = NDTI)) +
+  xlab("Crop Type") +
+  ylab("Normalized Difference Tillage Index") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = PSRI, color = croptype))
+  geom_boxplot(aes(x = croptype, y = PSRI)) +
+  xlab("Crop Type") +
+  ylab("Plant Senescence Reflectance Index") +
+  theme_bw()
 
 ggplot(Crops) +
-  geom_boxplot(aes(x = croptype, y = SAVI, color = croptype))
+  geom_boxplot(aes(x = croptype, y = SAVI)) +
+  xlab("Crop Type") +
+  ylab("Soil Adjusted Vegetation Index") +
+  theme_bw()
